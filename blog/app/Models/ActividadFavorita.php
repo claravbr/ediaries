6fbf0fdf97fad2c577f5f3b6e2623bffa19bf5c2
claravbr <clara.vbr@gmail.com>
2023-04-30 01:54:23 +0200

    ED-7  Clases del modelo de datos
    Creación de las clases del modelo de datos, algunas con el atributo y algunas solo la clase

---
+++ blog/app/Models/ActividadFavorita.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ActividadFavorita extends Model
+{
+    use HasFactory;
+
+    protected $table = 'actividades_favoritas';
+
+    protected $fillable = [
+        'nombre'
+    ];
+
+    /**
+     * The children that belong to the actividadfavorita
+     */
+    public function children()
+    {
+        return $this->belongsToMany(Child::class, 'child_actividad_favorita', 'actividad_favorita_id', 'child_id');
+    }
+}
--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\Usuario::factory(10)->create();
     }
 }
